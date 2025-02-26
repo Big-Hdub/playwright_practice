ee3af8b17472c9fff51510a39c77c5eb3aec8388
Big-Hdub <hdub-bb@hotmail.com>
2025-02-25 21:03:57 -0800

    Refactor match scraper to optimize performance and streamline data processing

--- utils.js
+++ utils.js
@@ -32,6 +32,7 @@ const matchScraper = async ({ url, page }) => {
         const teams = await page.locator('[class*="team-selector-team"]').all();
         let away = true;
         let awayTeam = {};
+        let awayName;
 
         const awayScoresRow = await page.locator('tr[class*="linescore-team-away"]').first();
         const homeScoresRow = await page.locator('tr[class*="linescore-team-home"]').first();
@@ -48,7 +49,7 @@ const matchScraper = async ({ url, page }) => {
 
         for (let teamStats of teams) {
             await teamStats.click({ waitUntil: 'load' });
-            await page.waitForTimeout(500);
+            await page.waitForTimeout(1000);
 
             let ts;
             const teamName = (await teamStats.textContent()).trim();
@@ -64,6 +65,7 @@ const matchScraper = async ({ url, page }) => {
             ts = data.teams[teamName].teamStats;
 
             const team = { name: teamName, players: {}, matchStats: statObjectCreator() };
+            const ms = team.matchStats;
 
             const table = await page.locator('[class*="boxscore-table_player"] tbody');
             const playerRows = await table.locator('tr').all();
@@ -84,26 +86,26 @@ const matchScraper = async ({ url, page }) => {
                     const pStats = data.teams[teamName].players[stats[0]];
                     pStats[5] = (pStats[2] - pStats[3]) / pStats[4];
                 }
-                team.matchStats.assists += +stats[7];
-                team.matchStats.aces += +stats[8];
-                team.matchStats.serveErrors += +stats[9];
-                team.matchStats.receiveError += +stats[10];
-                team.matchStats.digs += +stats[11];
-                team.matchStats.blockSolo += +stats[12];
-                team.matchStats.blockAssist += +stats[13];
-                team.matchStats.blockError += +stats[14];
-                team.matchStats.ballHandlingError += +stats[15];
-                team.matchStats.earnedPoints += +stats[16];
-                data.teams[teamName].teamStats.assists += +stats[7];
-                data.teams[teamName].teamStats.aces += +stats[8];
-                data.teams[teamName].teamStats.serveErrors += +stats[9];
-                data.teams[teamName].teamStats.receiveError += +stats[10];
-                data.teams[teamName].teamStats.digs += +stats[11];
-                data.teams[teamName].teamStats.blockSolo += +stats[12];
-                data.teams[teamName].teamStats.blockAssist += +stats[13];
-                data.teams[teamName].teamStats.blockError += +stats[14];
-                data.teams[teamName].teamStats.ballHandlingError += +stats[15];
-                data.teams[teamName].teamStats.earnedPoints += +stats[16];
+                ms.assists += +stats[7];
+                ms.aces += +stats[8];
+                ms.serveErrors += +stats[9];
+                ms.receiveError += +stats[10];
+                ms.digs += +stats[11];
+                ms.blockSolo += +stats[12];
+                ms.blockAssist += +stats[13];
+                ms.blockError += +stats[14];
+                ms.ballHandlingError += +stats[15];
+                ms.earnedPoints += +stats[16];
+                ts.assists += +stats[7];
+                ts.aces += +stats[8];
+                ts.serveErrors += +stats[9];
+                ts.receiveError += +stats[10];
+                ts.digs += +stats[11];
+                ts.blockSolo += +stats[12];
+                ts.blockAssist += +stats[13];
+                ts.blockError += +stats[14];
+                ts.ballHandlingError += +stats[15];
+                ts.earnedPoints += +stats[16];
             }
 
             const setRows = (await page.locator('[class*="boxscore-table_set"] tbody tr').all());
@@ -114,62 +116,85 @@ const matchScraper = async ({ url, page }) => {
                 if (away) data.games[url].sets.away[+set[0]] = set.slice(1);
                 else data.games[url].sets.home[+set[0]] = set.slice(1);
 
-                team.matchStats.setsPlayed++;
-                team.matchStats.kills += +(set[1]);
-                team.matchStats.errors += +(set[2]);
-                team.matchStats.totalAttacks += +(set[3]);
-                data.teams[teamName].teamStats.setsPlayed++;
-                data.teams[teamName].teamStats.kills += +(set[1]);
-                data.teams[teamName].teamStats.errors += +(set[2]);
-                data.teams[teamName].teamStats.totalAttacks += +(set[3]);
+                ms.setsPlayed++;
+                ms.kills += +(set[1]);
+                ms.errors += +(set[2]);
+                ms.totalAttacks += +(set[3]);
+                ts.setsPlayed++;
+                ts.kills += +(set[1]);
+                ts.errors += +(set[2]);
+                ts.totalAttacks += +(set[3]);
             }
 
-            team.matchStats.killsPerSet = Math.round(((team.matchStats.kills / team.matchStats.setsPlayed) * 100) / 100)
-            team.matchStats.hittingPercentage = Math.round(((team.matchStats.kills - team.matchStats.errors) / team.matchStats.totalAttacks) * 1000) / 1000;
-            team.matchStats.assistsPerSet = Math.round(((team.matchStats.assists / team.matchStats.setsPlayed) * 100) / 100);
-            team.matchStats.acesPerSet = Math.round(((team.matchStats.aces / team.matchStats.setsPlayed) * 100) / 100)
-            team.matchStats.digsPerSet = Math.round(((team.matchStats.digs / team.matchStats.setsPlayed) * 100) / 100)
-            team.matchStats.blocksPerSet = Math.round((((team.matchStats.blockSolo + team.matchStats.blockAssist) - team.matchStats.blockError / team.matchStats.setsPlayed) * 100) / 100);
-            team.matchStats.earnedPointsPerSet = Math.round(((team.matchStats.earnedPoints / team.matchStats.setsPlayed) * 100) / 100);
-            team.matchStats.errorsPerSet = Math.round(((team.matchStats.errors + team.matchStats.blockError + team.matchStats.ballHandlingError / team.matchStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.killsPerSet = Math.round(((data.teams[teamName].teamStats.kills / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.hittingPercentage = Math.round(((data.teams[teamName].teamStats.kills - data.teams[teamName].teamStats.errors) / data.teams[teamName].teamStats.totalAttacks) * 1000) / 1000;
-            data.teams[teamName].teamStats.assistsPerSet = Math.round(((data.teams[teamName].teamStats.assists / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.acesPerSet = Math.round(((data.teams[teamName].teamStats.aces / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.digsPerSet = Math.round(((data.teams[teamName].teamStats.digs / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.blocksPerSet = Math.round((((data.teams[teamName].teamStats.blockSolo + data.teams[teamName].teamStats.blockAssist) - data.teams[teamName].teamStats.blockError / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.earnedPointsPerSet = Math.round(((data.teams[teamName].teamStats.earnedPoints / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.errorsPerSet = Math.round(((data.teams[teamName].teamStats.errors + data.teams[teamName].teamStats.blockError + data.teams[teamName].teamStats.ballHandlingError / data.teams[teamName].teamStats.setsPlayed) * 100) / 100);
-            data.teams[teamName].teamStats.matchesPlayed++;
+            ms.killsPerSet = Math.round((ms.kills / ms.setsPlayed) * 100) / 100;
+            ms.hittingPercentage = Math.round(((ms.kills - ms.errors) / ms.totalAttacks) * 1000) / 1000;
+            ms.assistsPerSet = Math.round((ms.assists / ms.setsPlayed) * 100) / 100;
+            ms.acesPerSet = Math.round((ms.aces / ms.setsPlayed) * 100) / 100;
+            ms.digsPerSet = Math.round((ms.digs / ms.setsPlayed) * 100) / 100;
+            ms.blocksPerSet = Math.round(((ms.blockSolo + (ms.blockAssist / 2)) / ms.setsPlayed) * 100) / 100;
+            ms.earnedPointsPerSet = Math.round((ms.earnedPoints / ms.setsPlayed) * 100) / 100;
+            ms.errorsPerSet = Math.round((ms.errors + ms.blockError + ms.ballHandlingError / ms.setsPlayed) * 100) / 100;
+            ts.killsPerSet = Math.round((ts.kills / ts.setsPlayed) * 1000) / 1000;
+            ts.hittingPercentage = Math.round(((ts.kills - ts.errors) / ts.totalAttacks) * 1000) / 1000;
+            ts.assistsPerSet = Math.round((ts.assists / ts.setsPlayed) * 1000) / 100;
+            ts.acesPerSet = Math.round((ts.aces / ts.setsPlayed) * 100) / 100;
+            ts.digsPerSet = Math.round((ts.digs / ts.setsPlayed) * 100) / 100;
+            ts.blocksPerSet = Math.round(((ts.blockSolo + (ts.blockAssist / 2)) / ts.setsPlayed) * 100) / 100;
+            ts.earnedPointsPerSet = Math.round((ts.earnedPoints / ts.setsPlayed) * 100) / 100;
+            ts.errorsPerSet = Math.round((ts.errors + ts.blockError + ts.ballHandlingError / ts.setsPlayed) * 100) / 100;
+            ts.matchesPlayed++;
 
             if (away) {
-                team.matchStats.setWins = scores.away[scores.away.length - 1];
-                team.matchStats.setLosses = scores.home[scores.home.length - 1];
-                team.matchStats.setWinPercentage = team.matchStats.setWins / team.matchStats.setsPlayed;
-                data.teams[teamName].teamStats.setWins += scores.away[scores.away.length - 1];
-                data.teams[teamName].teamStats.setLosses += +(scores.home[scores.home.length - 1]);
-                data.teams[teamName].teamStats.setWinPercentage = data.teams[teamName].teamStats.setWins / data.teams[teamName].teamStats.setsPlayed;
-                if ((scores.away[scores.away.length - 1]) === 3) data.teams[teamName].teamStats.matchWins++;
-                else data.teams[teamName].teamStats.matchLosses++;
+                ms.setWins = scores.away[scores.away.length - 1];
+                ms.setLosses = scores.home[scores.home.length - 1];
+                ms.setWinPercentage = ms.setWins / ms.setsPlayed;
+                ts.setWins += scores.away[scores.away.length - 1];
+                ts.setLosses += +(scores.home[scores.home.length - 1]);
+                ts.setWinPercentage = ts.setWins / ts.setsPlayed;
+                if ((scores.away[scores.away.length - 1]) === 3) ts.matchWins++;
+                else ts.matchLosses++;
                 awayTeam = team;
+                awayName = teamName;
                 away = false;
             } else {
-                team.matchStats.setWins = scores.home[scores.home.length - 1];
-                team.matchStats.setLosses = scores.away[scores.away.length - 1];
-                team.matchStats.setWinPercentage = team.matchStats.setWins / team.matchStats.setsPlayed;
-                data.teams[teamName].teamStats.setWins += (scores.home[scores.home.length - 1]);
-                data.teams[teamName].teamStats.setLosses += (scores.away[scores.away.length - 1]);
-                data.teams[teamName].teamStats.setWinPercentage = data.teams[teamName].teamStats.setWins / data.teams[teamName].teamStats.setsPlayed;
-                if ((scores.home[scores.home.length - 1]) === 3) data.teams[teamName].teamStats.matchWins++;
-                else data.teams[teamName].teamStats.matchLosses++;
-                data.teams[teamName].teamStats.matchWinPercentage = data.teams[teamName].teamStats.matchWins / data.teams[teamName].teamStats.matchesPlayed;
+                ms.setWins = scores.home[scores.home.length - 1];
+                ms.setLosses = scores.away[scores.away.length - 1];
+                ms.setWinPercentage = ms.setWins / ms.setsPlayed;
+                ts.setWins += (scores.home[scores.home.length - 1]);
+                ts.setLosses += (scores.away[scores.away.length - 1]);
+                ts.setWinPercentage = ts.setWins / ts.setsPlayed;
+                if ((scores.home[scores.home.length - 1]) === 3) ts.matchWins++;
+                else ts.matchLosses++;
+                ts.matchWinPercentage = ts.matchWins / ts.matchesPlayed;
+
+                const awayMs = awayTeam.matchStats;
+                ms.opponentKills = awayMs.kills;
+                ms.opponentErrors = awayMs.errors;
+                ms.opponentTotalAttacks = awayMs.totalAttacks;
+                ms.opponentHittingPercentage = awayMs.hittingPercentage;
+                ms.opponentAces = awayMs.aces;
+                ts.opponentKills += awayMs.kills;
+                ts.opponentErrors += awayMs.errors;
+                ts.opponentTotalAttacks += awayMs.totalAttacks;
+                ts.opponentAces += awayMs.aces;
+                ts.opponentHittingPercentage = Math.round(((ts.kills - ts.errors) / ts.totalAttacks) * 1000) / 1000;
+                awayMs.opponentKills = ms.kills;
+                awayMs.opponentErrors = ms.errors;
+                awayMs.opponentTotalAttacks = ms.totalAttacks;
+                awayMs.opponentHittingPercentage = ms.hittingPercentage;
+                awayMs.opponentAces = ms.aces;
+                const awayTs = data.teams[awayName].teamStats;
+                awayTs.opponentKills += ms.kills;
+                awayTs.opponentErrors += ms.errors;
+                awayTs.opponentTotalAttacks += ms.totalAttacks;
+                awayTs.opponentAces += ms.aces;
+                awayTs.opponentHittingPercentage = Math.round(((awayTs.kills - awayTs.errors) / awayTs.totalAttacks) * 1000) / 1000;
             }
             data.games[url].teams.push(team);
-            data.teams[teamName].teamStats.matches.push(url);
-            data.teams[teamName].teamStats.matchWinPercentage = data.teams[teamName].teamStats.matchWins / data.teams[teamName].teamStats.matchesPlayed
+            ts.matches.push(url);
+            ts.matchWinPercentage = ts.matchWins / ts.matchesPlayed
         }
         fs.writeFileSync('./data/mensVB.json', JSON.stringify(data, null, 2), 'utf-8');
-        await page.pause();
     }
 
     return await page.close();
